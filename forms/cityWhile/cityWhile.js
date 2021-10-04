a18bd8e5c09c4caef4cd874a4015accb83086cf8
JackRyanCaton <jrc88858@creighton.edu>
2021-10-03 23:27:30 -0500

    compare

--- forms/cityWhile/cityWhile.js
+++ forms/cityWhile/cityWhile.js
@@ -1,9 +1,6 @@
-let cities = []
+/*let cities = []
 let lastCity = false
-<<<<<<< HEAD
 
-=======
->>>>>>> 4b56826592d044c8a117bc085788cc3c63ea890b
 while (lastCity == false) {
   userAnswer = prompt("Enter another city or say No")
   if (userAnswer == "No")
@@ -16,4 +13,4 @@ let i = 0
 
 while (i < cities.length) {
   console.log(cities[i].toLowerCase())
-  i++; }
+  i++; }*/
